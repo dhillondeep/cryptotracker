b92ae4013f4b48d43504c14f790298b02b079eda
Deep Dhillon <deep@deepdhillon.ca>
2018-03-18 14:41:54 -0400

    coin, date and information packets created

--- main.go
+++ main.go
@@ -1,29 +1,57 @@
 package main
 
+import (
+	cmc "github.com/miguelmota/go-coinmarketcap"
+	"time"
+	"strconv"
+)
+
 // date for the coins
 type Date struct {
-	Year int
-	Month int
-	Day int
-	Time int
-}
-
-// coin information
-type Coin struct {
-	Name string
-	Price int
-	MarketCap int
-	Volume int
-	CirculatingSupply int
+	Year  int
+	Month string
+	Day   int
+	Time  string
 }
 
 // total packet
 type Information struct {
 	Date Date
-	Coin Coin
+	Coin cmc.Coin
 	News []string
 }
 
 func main() {
 	return
+}
+
+// Creates a coin information packet that contains the date, coin and news
+func getCoinInformation(coinName string) (Information) {
+	return Information{
+		getDate(),
+		getCoin(coinName),
+		[]string{},
+	}
+}
+
+// Gets current date and converts it into struct which will be later on be
+// used in JSON
+func getDate() (Date) {
+	currTime := time.Now()
+
+	return Date{
+		currTime.Year(),
+		currTime.Month().String(),
+		currTime.Day(),
+		strconv.Itoa(currTime.Hour()) + ":" + strconv.Itoa(currTime.Minute()) + ":" +
+			strconv.Itoa(currTime.Second()),
+	}
+}
+
+// Gathers data from CoinMarketCap and returns the coin struct
+// This struct will later be converted into JSON
+func getCoin(name string) (cmc.Coin) {
+	coinInfo, _ := cmc.GetCoinData(name)
+
+	return coinInfo
 }
